34755fce33c50f65817e1df53ce07a304fb3fbd7
tiendev <ltien540@gmail.com>
2022-07-29 11:34:53 +0700

    run app in linux environment

--- app.js
+++ app.js
@@ -4,24 +4,64 @@ const puppeteer = require("puppeteer");
 const PORT = process.env.PORT;
 const app = express();
 const cron = require("node-cron");
+const cors = require("cors");
 
 const https = require("https");
 const axios = require("axios");
 const chromium = require("chrome-aws-lambda");
+app.use(cors());
+
+const { exec } = require("child_process");
 
 const sendMsgToTelegram = async (msg) => {
   const url =
     process.env.BASE_URL +
     process.env.TOKEN +
     `/sendmessage?chat_id=${process.env.CHAT_ID.toString()}&text=${msg.toString()}`;
-  const uri = url.toString();
-  return await https.get(uri);
-  // return await axios.get(uri);
+  let uri = url.toString();
+  // return await https.get(uri, (res) => {
+  //   let data = [];
+  //   const headerDate =
+  //     res.headers && res.headers.date ? res.headers.date : "no response date";
+  //   console.log("Status Code:", res.statusCode);
+  //   console.log("Date in Response header:", headerDate);
+  //   console.log("res :", res);
+  //   res.on("data", (chunk) => {
+  //     data.push(chunk);
+  //     console.log("data ", data);
+  //   });
+  // });
+  return await axios.get(uri);
+};
+
+const sendMsgTelegramByShellScrip = (msg) => {
+  exec(`bash  ./app.sh "${msg}"`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
 };
 
 const checkInOut = async () => {
   const timeElapsed = Date.now();
   const today = new Date(timeElapsed);
+  let IN_OR_OUT = "";
+  const currentHours = today.getHours();
+  if (currentHours == 9) {
+    IN_OR_OUT = "in";
+  } else if (currentHours == 18) {
+    IN_OR_OUT = "out";
+  }
+
+    console.log("calling to my bot telegram ...............");
+    console.log("bot said :");
+
   try {
     const browser = await puppeteer.launch({
       headless: true,
@@ -36,7 +76,7 @@ const checkInOut = async () => {
     await page.focus("#log-userid");
     await page.type("#log-userid", process.env.USERID);
 
-    await autoScroll(page);
+    // await autoScroll(page);
     await delay(page);
 
     const frameHandle = await page.$("iframe[id='iframeLoginPassword']");
@@ -51,26 +91,25 @@ const checkInOut = async () => {
     await page.waitForSelector(
       ".btn.btn-danger.btn-round.no-border.width-100.btn-sm"
     );
-    //await page.click(".btn.btn-danger.btn-round.no-border.width-100.btn-sm");
+    await page.click(".btn.btn-danger.btn-round.no-border.width-100.btn-sm");
+    await delay(page);
     await browser.close();
-    console.log("calling to my bot telegram ...............");
-    console.log("bot said :");
-    const msg = today.toUTCString().concat("\tcheck in-out sucess");
-    await sendMsgToTelegram(msg)
-      .then(() => console.log("check in-out sucess"))
-      .catch((error) => console.log("send message to telegram false :", error));
+
+    const msg = today.toUTCString().concat(`\t check ${IN_OR_OUT} sucess`);
+    sendMsgTelegramByShellScrip(msg.toString());
+    console.log(`check ${IN_OR_OUT} sucess`);
   } catch (error) {
-    const msg = today.toUTCString().concat("\tcheck in-out fail");
-    await sendMsgToTelegram(msg)
-      .then(() => console.log("check in-out fail"))
-      .catch((error) => console.log("send message to telegram false :", error));
+    const msg = today.toUTCString().concat(`\t check ${IN_OR_OUT} fail`);
+    sendMsgTelegramByShellScrip(msg);
+    console.log(`check ${IN_OR_OUT} fail`);
+    console.error(error);
   }
 };
 
 const delay = async (page) => {
   await page.evaluate(async (time) => {
     await new Promise(function (resolve) {
-      setTimeout(resolve, 3000);
+      setTimeout(resolve, 4000);
     });
   });
 };
@@ -95,9 +134,9 @@ const autoScroll = async (page) => {
 };
 
 app.listen(PORT, () => {
-  cron.schedule("0 0 15,18 * * 1-5", () => {
+  cron.schedule("0 0 9,18 * * 1-5", async () => {
     console.log("Scheduler running ............");
-    checkInOut();
+    await checkInOut();
     console.log(".......................end job");
   });
   // checkInOut();
